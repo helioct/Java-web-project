6bc35d62ca30ad47084088d5d382a88b4866158e
alfonsomg11 <67347988+alfonsomg11@users.noreply.github.com>
2020-11-14 16:11:46 -0600

    Add Products REST service

---
+++ OnlineShop_Productos/src/main/java/com/deloitte/logic/ProductoUser.java
@@ -0,0 +1,38 @@
+package com.deloitte.logic;
+
+
+
+public class ProductoUser {
+
+
+	private String descripcion;
+	private double precio;
+	private String categoria;
+	
+	public ProductoUser() {
+		
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+	
+
+}
